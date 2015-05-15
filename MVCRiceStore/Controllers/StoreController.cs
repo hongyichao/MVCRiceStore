fca463375f56f6a7c053ffc4827dfc01e5585992
hongyi <lucashyc@gmail.com>
2015-05-15 17:03:08 +1000

    store Index, Create

---
+++ MVCRiceStore/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCRiceStore.Models;
+
+namespace MVCRiceStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(db.stores.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Store s)
+        {
+            if(ModelState.IsValid)
+            {
+                db.stores.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(s);
+        }
+    }
+}
