ee1e255bc88e373c0a52ecf03351ae71996ecd0f
hongyichao <lucaschao@outlook.com>
2015-06-14 22:25:37 +1000

    change store views' titles and disable list box in store delete view

--- MVCRiceStore/App_Start/RouteConfig.cs
+++ MVCRiceStore/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCRiceStore
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
